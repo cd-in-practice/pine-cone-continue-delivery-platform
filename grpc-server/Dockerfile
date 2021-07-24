8297addb7b771d09ef1a2d65195c623437aae5c3
zacker330 <zacker330@gmail.com>
2021-07-24 17:14:11 +0800

    fix: config mainClass

---
+++ grpc-server/Dockerfile
@@ -0,0 +1,3 @@
+FROM openjdk:11.0.12-jre-buster
+COPY target/app.jar /app.jar
+CMD java -jar $JAVA_OPT /app.jar
--- grpc-server/src/main/java/codes/showme/pinecone/cdp/grpcserver/Main.java
+++ grpc-server/src/main/java/codes/showme/pinecone/cdp/grpcserver/Main.java
@@ -10,6 +10,7 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan({"codes.showme.pinecone.cdp"})
+@SpringBootApplication
 public class Main implements ApplicationContextAware {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
