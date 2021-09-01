1cc044fceaa6037900bb7b15c0176e08fdded590
zacker330 <zacker330@gmail.com>
2021-09-02 07:03:27 +0800

    ignore unreasonable test

--- grpc-server/src/test/java/codes/showme/pinecone/cdp/TarTest.java
+++ grpc-server/src/test/java/codes/showme/pinecone/cdp/TarTest.java
@@ -5,12 +5,14 @@ import code.showme.pinecone.cdp.artifact.TarArtifactGrpc;
 import code.showme.pinecone.cdp.artifact.TarArtifactOuterClass;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import org.junit.Ignore;
 
 import java.util.logging.Level;
 
 public class TarTest {
 
     @org.junit.Test
+    @Ignore
     public void name() throws InterruptedException {
         ManagedChannelBuilder<?> channelBuilder = ManagedChannelBuilder.forAddress("127.0.0.1", 6565);
         ManagedChannel channel = channelBuilder.usePlaintext().build();
