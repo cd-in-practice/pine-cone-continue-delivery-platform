94416726ef98b0890600bc2cf245b3a2f898fc2a
zacker330 <zacker330@gmail.com>
2021-10-10 07:57:55 +0800

    feat: create alert server

---
+++ cdp-alert-server/src/main/java/codes/showme/pinecone/cdp/alert/controller/AliyunAlertController.java
@@ -0,0 +1,12 @@
+package codes.showme.pinecone.cdp.alert.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller()
+@RequestMapping(value = "/aliyun")
+public class AliyunAlertController {
+
+
+
+}
