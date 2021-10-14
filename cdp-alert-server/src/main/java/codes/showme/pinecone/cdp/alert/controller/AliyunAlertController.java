28ecfe1053eb29c5ede977b16be798a27920f5cb
zacker330 <zacker330@gmail.com>
2021-10-14 23:23:31 +0800

    feat: init alert receive module

--- cdp-alert-server/src/main/java/codes/showme/pinecone/cdp/alert/controller/AliyunAlertController.java
+++ cdp-alert-server/src/main/java/codes/showme/pinecone/cdp/alert/controller/AliyunAlertController.java
@@ -1,12 +1,18 @@
 package codes.showme.pinecone.cdp.alert.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller()
+import java.util.Map;
+
+@RestController
 @RequestMapping(value = "/aliyun")
 public class AliyunAlertController {
 
+    @RequestMapping("/receive")
+    public String receive(@RequestParam(required = false) Map<String, String> allParams,
+                          @RequestHeader(required = false) Map<String, String> allHeaders, @RequestBody(required = false) String body) {
+        return "ok";
+    }
 
 
 }
