edeeff8a77a16a6248cbc1d390288d0d12f03145
zacker330 <zacker330@gmail.com>
2022-01-02 07:10:43 +0800

    move cdp-alert-server to cdp-outalator-server

--- cdp-outalator-server/src/main/java/codes/showme/pinecone/cdp/outalator/Main.java
+++ cdp-outalator-server/src/main/java/codes/showme/pinecone/cdp/outalator/Main.java
@@ -10,7 +10,7 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"codes.showme.pinecone.cdp.alert"})
+@ComponentScan({"codes.showme.pinecone.cdp.outalator"})
 public class Main implements ApplicationContextAware {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
